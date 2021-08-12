2c09f78f6e63e8c2b42f245917fbb4bddd3f4710
Kdavydkin <88538824+Kdavydkin@users.noreply.github.com>
2021-08-12 18:01:28 +0300

    Add files via upload

---
+++ lesson_2/exception/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson_2.exception;
+
+public class MyArraySizeException extends CustomException {
+
+    public MyArraySizeException() {
+        super("The matrix must be of size 4x4");
+    }
+}
